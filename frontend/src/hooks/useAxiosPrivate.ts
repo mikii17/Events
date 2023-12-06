76d8157fb5b70fe248390493990dc0eaf0fa748d
mikii17 <114937644+mikii17@users.noreply.github.com>
2023-12-06 22:29:25 +0300

    Add private axios client hook

---
+++ frontend/src/hooks/useAxiosPrivate.ts
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import axios_client from "../api/axios_client";
+
+type RefreshFunction = () => Promise<string>;
+const useAxiosPrivate = ({
+  token,
+  refresh,
+}: {
+  token: string;
+  refresh: RefreshFunction;
+}) => {
+  useEffect(() => {
+    const requestInterceptor = axios_client.interceptors.request.use(
+      (config) => {
+        if (!config.headers["Authorization"]) {
+          config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
+    const responseInterceptor = axios_client.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error?.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+          originalRequest._retry = true;
+          const access_token = await refresh();
+          axios_client.defaults.headers.common["Authorization"] =
+            "Bearer " + access_token;
+          return axios_client(originalRequest);
+        }
+        return Promise.reject(error);
+      }
+    );
+    return () => {
+      axios_client.interceptors.request.eject(requestInterceptor);
+      axios_client.interceptors.response.eject(responseInterceptor);
+    };
+  }, [token, refresh]);
+
+  return axios_client;
+};
+export default useAxiosPrivate;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,33 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import './index.css';
-import Home from './pages/Home.tsx';
-import Event from './pages/Event.tsx';
-import Register from './pages/SignUp.tsx';
-import Error from './pages/Error.tsx';
-import App from './App.tsx';
-import Signup from './pages/auth/Signup.tsx';
-import ChangePassword from './pages/auth/ChangePassword.tsx';
-import Login from './pages/auth/Login.tsx';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home.tsx";
+import Event from "./pages/Event.tsx";
+import Register from "./pages/SignUp.tsx";
+import Error from "./pages/Error.tsx";
+import App from "./App.tsx";
+import Signup from "./pages/auth/Signup.tsx";
+import ChangePassword from "./pages/auth/ChangePassword.tsx";
+import Login from "./pages/auth/Login.tsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <Error />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/event/:id',
+        path: "/event/:id",
         element: <Event />,
       },
       {
-        path: '/signup',
+        path: "/signup",
         element: <Register />,
       },
     ],
@@ -36,17 +41,20 @@ const router = createBrowserRouter([
 
 const router1 = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App/>} errorElement={<Error/ >}>
-      <Route index element={<Home/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/change-password' element={<ChangePassword/>}/>
-      <Route path='/event/:id' element={<Event/>}/>
-      <Route path='/register/:id' element={<Register/>}/>
+    <Route path="/" element={<App />} errorElement={<Error />}>
+      <Route index element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/event/:id" element={<Event />} />
+      <Route path="/register/:id" element={<Register />} />
+      <Route>
+        <Route path="/signup" element={<Signup />} />
+      <Route path="/change-password" element={<ChangePassword />} />
+      </Route>
+      <Route path="*" element={<Error />} /> // TODO: 404 page
     </Route>
   )
 );
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router1} />
   </React.StrictMode>
